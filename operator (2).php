48b1aec4f8fbb26d09b38c82aedee50d0963e684
nandarphonemyint <169057533+nandarphonemyint@users.noreply.github.com>
2024-05-07 14:28:41 +0630

    Add files via upload

---
+++ operator (2).php
@@ -0,0 +1,12 @@
+<?php
+$num1=50;
+$num2= $num1+10;
+$num3= $num1-10;
+$num4= $num1*10;
+$num5= $num1/10;
+echo $num1."<br>";
+echo $num2."<br>";
+echo $num3."<br>";
+echo $num4."<br>";
+echo $num5."<br>";
+?>
